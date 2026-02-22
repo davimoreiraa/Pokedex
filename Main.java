e5c4d9641f17539881cefed96abd3313f5a2d72d
Davi <davimoreiraasantoss@gmail.com>
2026-02-22 15:05:36 -0300

    project started

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        ArrayList<String> pokedex = new ArrayList<>(); 
+        pokedex.add("Charizard");
+        pokedex.add("Lucario");
+        int option = 0;
+
+        while (option != 3) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1 - Visualize Pokemons in the Pokedex");
+            System.out.println("2 - Insert a Pokemon in the Pokedex");
+            // System.out.println("3 - Delete Pokemon");
+            // System.out.println("3 - Edit Pokemon");
+            System.out.println("3 - Quite");
+            
+            option = terminal.nextInt(); // collect the digit 
+            terminal.nextLine(); // clear terminal
+
+            switch (option) {
+                case 1:
+                    System.out.println(" \nPokemons:");
+                    for (String pokemon: pokedex) {
+                        System.out.println("-> "+ pokemon);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Pokemon's name:");
+                    String name = terminal.nextLine(); //collect the name
+                    pokedex.add(name);
+                    break;
+                case 3:
+                    System.out.println("Closing program...");
+                    break;
+            }
+        }
+    }
+}
